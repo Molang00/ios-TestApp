faae174ffb4ebdc4c3e82ed51384e52bd5e99f0d
byeongsu kim <bskim@autocrypt.io>
2021-01-19 16:22:25 +0900

    first commit

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var mainButton2: UIButton!
+    
+    var mainLabel2: UILabel!
+    
+    @IBAction func setText(_ sender: UIButton) {
+        if sender.isEqual(mainButton){
+            mainLabel.text = "success"
+        } else {
+            mainLabel.text = "fail"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainLabel.text = "Test"
+        mainButton.setTitle("buttonTest", for: .highlighted)
+        mainButton2.setTitle("buttonTest2", for: .highlighted)
+        
+        mainLabel2 = UILabel(frame: CGRect(x: 150,
+                                           y: 150,
+                                           width: 30,
+                                           height: 30))
+        view.addSubview(mainLabel2)
+        mainLabel2.backgroundColor = UIColor.green
+        
+        sleep(1)
+        mainLabel2.text = "hi"
     }
 
 
